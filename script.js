b921909cc89686c0da44564f979a42e9137eee91
Weliton123 <welitonsousaex@gmail.com>
2023-09-16 17:10:37 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+let h2 = document.querySelector('h2');
+
+function success(pos){
+  console.log(pos.coords.latitude, pos.coords.longitude);
+  h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+}  
+function error(err){
+  console.log(err);
+};
+navigator.geolocation.getCurrentPosition(success);
